c6e09a9ce6c9eec2f2bacb7cc19a437ee998ba68
Christopher Bishop <domingobishop@gmail.com>
2015-11-22 15:29:31 +0000

    added links to home page

--- index.php
+++ index.php
@@ -10,17 +10,17 @@
             </div>
             <div class="row top-buffer">
                 <div class="col-sm-12 col-md-4 col-lg-4">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/home-fishing-report.jpg" alt="Tackle World" class="img-responsive">
+                    <a href="http://tackleworldpl.com.au/fishing-report/" title="FISHING REPORT"><img src="<?php echo get_template_directory_uri(); ?>/img/home-fishing-report.jpg" alt="Tackle World" class="img-responsive"></a>
                     <h3>FISHING REPORT</h3>
                     <p>Click here to get Today’s Fishing Report for the Port Lincoln area.</p>
                 </div>
                 <div class="col-sm-12 col-md-4 col-lg-4">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/home-port-lincoln.jpg" alt="Tackle World" class="img-responsive">
+                    <a href="http://tackleworldpl.com.au/port-lincoln-surrounds/" title="PORT LINCOLN AND SURROUNDS"><img src="<?php echo get_template_directory_uri(); ?>/img/home-port-lincoln.jpg" alt="Tackle World" class="img-responsive"></a>
                     <h3>PORT LINCOLN AND SURROUNDS</h3>
                     <p>Information about the area and handy links to all the sites to make planning your visit to the Eyre Peninsula easy.</p>
                 </div>
                 <div class="col-sm-12 col-md-4 col-lg-4">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/home-whats-catching.jpg" alt="Tackle World" class="img-responsive">
+                    <a href="http://tackleworldpl.com.au/whats-catching/" title="WHAT’s CATCHING"><img src="<?php echo get_template_directory_uri(); ?>/img/home-whats-catching.jpg" alt="Tackle World" class="img-responsive"></a>
                     <h3>WHAT’s CATCHING (formally Local Heros)</h3>
                     <p>A selection of the best shots uploaded our Facebook Page.</p>
                 </div>
