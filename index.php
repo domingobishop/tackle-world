8aece3c60021bdb49857fc946d9a62686249b89f
Christopher Bishop <domingobishop@gmail.com>
2015-12-01 09:31:42 +1100

    responsive fixes

--- index.php
+++ index.php
@@ -9,17 +9,17 @@
                 </div>
             </div>
             <div class="row top-buffer">
-                <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="col-sm-4 col-md-4 col-lg-4">
                     <a href="http://tackleworldpl.com.au/fishing-report/" title="FISHING REPORT"><img src="<?php echo get_template_directory_uri(); ?>/img/home-fishing-report.jpg" alt="Tackle World" class="img-responsive"></a>
                     <h3>Fishing Report</h3>
                     <p>Click here to get Today’s Fishing Report for the Port Lincoln area.</p>
                 </div>
-                <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="col-sm-4 col-md-4 col-lg-4">
                     <a href="http://tackleworldpl.com.au/port-lincoln-surrounds/" title="PORT LINCOLN AND SURROUNDS"><img src="<?php echo get_template_directory_uri(); ?>/img/home-port-lincoln.jpg" alt="Tackle World" class="img-responsive"></a>
                     <h3>Port Lincoln and Surrounds</h3>
                     <p>Information about the area and handy links to all the sites to make planning your visit to the Eyre Peninsula easy.</p>
                 </div>
-                <div class="col-sm-12 col-md-4 col-lg-4">
+                <div class="col-sm-4 col-md-4 col-lg-4">
                     <a href="http://tackleworldpl.com.au/whats-catching/" title="WHAT’s CATCHING"><img src="<?php echo get_template_directory_uri(); ?>/img/home-whats-catching.jpg" alt="Tackle World" class="img-responsive"></a>
                     <h3>What's Catching</h3>
                     <p>A snap shot of what's catching around the Eyre Peninsula.</p>
