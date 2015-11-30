436d8a68fac9f76fac4ec0aca6b1ac5f5fe682e0
Christopher Bishop <domingobishop@gmail.com>
2015-12-01 09:25:24 +1100

    responsive tables

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <footer id="footer" class="bc-footer">
     <div class="container">
         <div class="row bc-info top-buffer">
-            <div class="col-sm-12 col-md-4 col-lg-4 text-left">
+            <div class="col-sm-4 col-md-4 col-lg-4 text-left">
                 <div class="vip-club clearfix">
                     <a href="http://tackleworldpl.com.au/vip-club/" title="VIP Club">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/vip-club.png" alt="Tackle World"
@@ -12,14 +12,14 @@
                     </a>
                 </div>
             </div>
-            <div class="col-sm-12 col-md-4 col-lg-4 text-left">
+            <div class="col-sm-4 col-md-4 col-lg-4 text-left">
                 <p>
                     <small>Tackle World Port Lincoln<br>
                         copyright © <?php echo date("Y"); ?> <br>
                         created by <a href="http://creatistic.com.au/" target="_blank">creatistic</a></small>
                 </p>
             </div>
-            <div class="col-sm-12 col-md-4 col-lg-4 text-right">
+            <div class="col-sm-4 col-md-4 col-lg-4 text-right">
                 <ul class="social-buttons">
                     <li><a href="https://www.facebook.com/tackleworldportlincoln" title="Facebook" target="_blank"><img
                                 src="<?php echo get_template_directory_uri(); ?>/img/icon-fb.png" alt="Facebook"
