4261bba8a9bd1ff03d1ea4941f02bca5719d4d2e
Christopher Bishop <domingobishop@gmail.com>
2015-12-01 12:50:55 +1100

    added signup

--- footer.php
+++ footer.php
@@ -13,11 +13,25 @@
                 </div>
             </div>
             <div class="col-sm-4 col-md-4 col-lg-4 text-left">
-                <p>
-                    <small>Tackle World Port Lincoln<br>
-                        copyright © <?php echo date("Y"); ?> <br>
-                        created by <a href="http://creatistic.com.au/" target="_blank">creatistic</a></small>
-                </p>
+                <!-- Begin MailChimp Signup Form -->
+                <div id="mc_embed_signup">
+                    <form action="//tackleworldpl.us12.list-manage.com/subscribe/post?u=c72d4fb83ff3e18016438a905&amp;id=c1f14b0107" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll" class="signup">
+                            <div class="mc-field-group">
+                                <label for="mce-EMAIL"><h3>Subscribe to our mailing list</h3></label><br>
+                                <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="email address">
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c72d4fb83ff3e18016438a905_c1f14b0107" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default"></div>
+                        </div>
+                    </form>
+                </div>
+
+                <!--End mc_embed_signup-->
             </div>
             <div class="col-sm-4 col-md-4 col-lg-4 text-right">
                 <ul class="social-buttons">
@@ -28,6 +42,11 @@
                                 src="<?php echo get_template_directory_uri(); ?>/img/icon-in.png" alt="Instagram"
                                 class="img-responsive"></a></li>
                 </ul>
+                <p>
+                    <small>Tackle World Port Lincoln<br>
+                        copyright © <?php echo date("Y"); ?> <br>
+                        created by <a href="http://creatistic.com.au/" target="_blank">creatistic</a></small>
+                </p>
             </div>
         </div>
     </div>
