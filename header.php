a1abc795501915ea190f63d4d20d559b56963102
Christopher Bishop <domingobishop@gmail.com>
2018-03-05 18:10:33 +0000

    GA code

--- header.php
+++ header.php
@@ -9,6 +9,15 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-77952242-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-77952242-1');
+    </script>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
     <title>
